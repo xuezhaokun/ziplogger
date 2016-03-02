8125770e4714b81dbe3bcdb822844df6b1523c9f
Zhaokun Xue <helloxzk@gmail.com>
2016-03-02 02:00:57 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
 <?php
-/*
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'ziplogger');
-*/
-define('DB_SERVER', 'ziplogger.db.7674180.hostedresource.com');
-define('DB_USERNAME', 'ziplogger');
-define('DB_PASSWORD', 'Kirby#57');
-define('DB_DATABASE', 'ziplogger');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
 
 ?>
