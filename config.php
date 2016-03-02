80d586b986fd0cb77677fd87b38c9002360ae1be
Zhaokun Xue <helloxzk@gmail.com>
2016-03-02 02:09:00 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,5 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'ziplogger');
-
-
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
